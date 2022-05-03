426658ae90a61f9716be791b4163fbd79a967b13
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-03 16:11:55 -0400

    added trim

--- src/main/java/org/example/Superhero.java
+++ src/main/java/org/example/Superhero.java
@@ -7,11 +7,11 @@ public class Superhero {
         Scanner Superhero = new Scanner(System.in);
         System.out.println("Let's write a story about a superhero.");
         System.out.println("Who is the hero of our story?");
-        String name = Superhero.nextLine();
+        String name = Superhero.nextLine().trim();
         System.out.println("What is their superpower?");
-        String power = Superhero.nextLine();
+        String power = Superhero.nextLine().trim();
         System.out.println("Here is the story:");
-        System.out.println("There once was a superhero named " + name + " , who had the power of " + power + ".");
+        System.out.println("There once was a superhero named " + name + ", who had the power of " + power + ".");
         System.out.println("As they grew older, " + name + " saw that the world needed them. ");
         System.out.println(name +" used their ability to " + power + " to save the world.");
     }
